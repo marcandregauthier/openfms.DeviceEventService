b82f60a364c8265bfdf587c62d04d205c584e6bc
magauthier <magauthier@astus.com>
2020-05-31 15:20:36 -0400

    Code format

--- model/position.js
+++ model/position.js
@@ -1,13 +1,13 @@
 ﻿import mongoose from 'mongoose';
 
 const positionSchema = new mongoose.Schema({
-    AstusSerialNumber: {type: String},
-    Location:   {type: String},
-    Longitude:  {type: Number },
-    Latitude:   {type: Number},
-    Heading:    {type: Number},
-    Speed: { type: Number },
-    Source: { type: String },
+    DeviceID:    { type: String },
+    Location:    { type: String },
+    Longitude:   { type: Number },
+    Latitude:    { type: Number },
+    Heading:     { type: Number },
+    Speed:       { type: Number },
+    Source:      { type: String },
     CreatedDate: { type: Date }
 });
 
