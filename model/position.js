f91e7fcc7e49d3c68ba88a6cdce710a65f862fbe
magauthier <magauthier@astus.com>
2020-05-31 14:51:16 -0400

    Inital upload

---
+++ model/position.js
@@ -0,0 +1,14 @@
+﻿import mongoose from 'mongoose';
+
+const positionSchema = new mongoose.Schema({
+    AstusSerialNumber: {type: String},
+    Location:   {type: String},
+    Longitude:  {type: Number },
+    Latitude:   {type: Number},
+    Heading:    {type: Number},
+    Speed: { type: Number },
+    Source: { type: String },
+    CreatedDate: { type: Date }
+});
+
+export default mongoose.model('Position', positionSchema);
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+﻿import express from 'express';
+
+import health from './endpoints/health';
+import position from './endpoints/position';
+
+const router = express.Router();
+
+
+
+router.route('/health').get(health.get);
+
+router.route('/position/get').get(position.get);
+router.route('/position/add').post(position.add);
+router.route('/position/clearAll').get(position.clearAll);
+
+
+
+export default router;
