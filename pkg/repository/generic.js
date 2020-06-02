3f4b2a49c6971c7f65aeeb906f82b7ca166077c3
magauthier <magauthier@astus.com>
2020-06-01 22:10:54 -0400

    Cleanup

--- pkg/repository/generic.js
+++ pkg/repository/generic.js
@@ -1,4 +1,17 @@
-﻿const get = async (Model) => {
+﻿const find = async (Model, id) => {
+    console.log(`repository.find: ${id}`);
+
+    return await Model.findById(id, (error, entity) => {
+        if (error) {
+            return { error };
+        } else {
+            return entity;
+        }
+    });
+};
+
+
+const get = async (Model) => {
     console.log('repository.get');
 
     return await Model.find((error, items) => {
@@ -12,6 +25,7 @@
 
 const add = async (Model, jsonEntity) => {
     console.log(`repository.add: ${jsonEntity}`);
+
     jsonEntity.CreatedDate = new Date();
     if (!jsonEntity.Source)
         jsonEntity.Source = require('../../package.json').name;
@@ -25,7 +39,17 @@ const add = async (Model, jsonEntity) => {
             return { error: error };
         });
 };
-    
+
+const update = async (Model, jsonEntity) => {
+    console.log(`repository.update: ${jsonEntity}`);
+
+    jsonEntity.CreatedDate = new Date();
+    if (!jsonEntity.Source)
+        jsonEntity.Source = require('../../package.json').name;
+
+    return await Model.findOneAndUpdate({ "_id": jsonEntity._id }, jsonEntity, { new: true });
+};
+
 const clearAll = async (Model) => {
     console.log(`repository.clearAll`);
 
